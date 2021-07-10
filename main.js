506fab94db378320f51b10034385de70495d98dd
unknown <mohmmedtouhid007@gmail.com>
2021-07-10 18:54:16 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// Getting ids from DOMS
+var iconOne = document.getElementById("arrowUp")
+var iconTwo = document.getElementById("arrowUpTwo")
+var iconThree = document.getElementById("arrowUpThree")
+var iconFour = document.getElementById("arrowUpFour")
+var iconFive = document.getElementById("arrowUpFive")
+
+
+var ansOne = document.getElementById("ansOne")
+var ansTwo = document.getElementById("ansTwo")
+var ansThree = document.getElementById("ansThree")
+var ansFour = document.getElementById("ansFour")
+var ansFive = document.getElementById("ansFive")
+
+
+
+
+// Collapse Function
+
+iconOne.addEventListener('click', () =>{
+    iconOne.classList.toggle('icon-active')
+    ansOne.classList.toggle('p-active')
+});
+
+iconTwo.addEventListener('click', () =>{
+    iconTwo.classList.toggle('icon-active')
+    ansTwo.classList.toggle('p-active')
+});
+
+iconThree.addEventListener('click', () =>{
+    iconThree.classList.toggle('icon-active')
+    ansThree.classList.toggle('p-active')
+});
+
+iconFour.addEventListener('click', () =>{
+    iconFour.classList.toggle('icon-active')
+    ansFour.classList.toggle('p-active')
+});
+
+iconFive.addEventListener('click', () =>{
+    iconFive.classList.toggle('icon-active')
+    ansFive.classList.toggle('p-active')
+});
+
